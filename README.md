650da70520f0e726a839b097953cd7b5927dd367
Elton Gonçalves Gomes <eltongonc@gmail.com>
2023-04-07 03:37:38 +0200

    added netlify files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NgPasswordGenerator
+# Angular Password Generator
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.4.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
 import { Component } from '@angular/core';
 
+type CheckBoxType = 'letter' | 'number' | 'symbol';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-password-generator';
+  title = 'Password Generator';
+
+  length = 10;
+  hasLetters = true;
+  hasNumbers = false;
+  hasSymbols = false;
+
+  password = '';
+
+  onSetLength(e: Event) {
+    const val = +(e.target as HTMLInputElement).value;
+
+    this.length = val;
+  }
+
+  onCheck(type: CheckBoxType, e: Event) {
+    const { checked } = e.target as HTMLInputElement;
+
+    switch (type) {
+      case 'letter':
+        this.hasLetters = checked;
+        break;
+      case 'number':
+        this.hasNumbers = checked;
+        break;
+      case 'symbol':
+        this.hasSymbols = checked;
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  onGeneratePassword() {
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()';
+    let output = '';
+
+    let all = '';
+
+    if (this.hasLetters) {
+      all = all.concat(letters);
+    }
+
+    if (this.hasNumbers) {
+      all = all.concat(numbers);
+    }
+
+    if (this.hasSymbols) {
+      all = all.concat(symbols);
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      const char = all[Math.floor(Math.random() * all.length)];
+      output = `${output}${char}`;
+    }
+
+    this.password = output;
+  }
 }
